965b467195fff8d260a9c2ae94d5677a1a0ac91d
Cristobal Dominguez <cristobald@gmail.com>
2018-11-12 10:49:49 -0300

    Devise Root Routes Improvements

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,16 @@
 Rails.application.routes.draw do
   resources :companies
   devise_for :users
+
+  devise_scope :user do
+    authenticated :user do
+      root 'companies#index', as: :authenticated_root
+    end
+
+    unauthenticated do
+      root 'devise/sessions#new', as: :unauthenticated_root
+    end
+  end
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
