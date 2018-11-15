8644aa045517bbcd5f86851dd2a56922e053a0be
Cristobal Dominguez <cristobald@gmail.com>
2018-11-15 18:27:52 -0300

    Nested Routes Improvements

--- app/controllers/plants_controller.rb
+++ app/controllers/plants_controller.rb
@@ -1,41 +1,39 @@
 class PlantsController < ApplicationController
   before_action :set_plant, only: [:show, :edit, :update, :destroy]
-  before_action :set_company, only: [:show, :edit,:new, :update, :destroy]
-  before_action :set_companies, only: [:new, :edit, :index, :show]
-  before_action :set_countries, only: [:new, :edit, :index, :show]
-  before_action :set_discharge_points, only: [:new, :edit, :show]
+  before_action :set_company, only: [:show, :edit,:new, :create, :update, :destroy]
+  before_action :set_companies, only: :index
+  before_action :set_countries, only: [:new, :edit, :create, :update]
 
-  # GET /plants
-  # GET /plants.json
+  # GET companies/:company_id/plants
+  # GET companies/:company_id/plants.json
   def index
     @plants = Company.find(params[:company_id]).plants.active
     @company = Company.find(params[:company_id])
   end
 
-  # GET /plants/1
-  # GET /plants/1.json
+  # GET companies/:company_id/plants/1
+  # GET companies/:company_id/plants/1.json
   def show
   end
 
-  # GET /plants/new
+  # GET companies/:company_id/plants/new
   def new
-    @plant = Plant.new
-    @companies = Company.all
-  end
+    @plant = @company.plants.build
+   end
 
-  # GET /plants/1/edit
+  # GET companies/:company_id/plants/1/edit
   def edit
     @plant = Plant.find(params[:id])
   end
 
-  # POST /plants
-  # POST /plants.json
+  # POST companies/:company_id/plants
+  # POST companies/:company_id/plants.json
   def create
-    @plant = Plant.new(plant_params)
-    @plant.company_id = params[:company_id]
+    @plant = @company.plants.build(plant_params)
+
     respond_to do |format|
       if @plant.save
-        format.html { redirect_to company_plant_path(@plant), notice: 'Plant was successfully created.' }
+        format.html { redirect_to [@company, @plant], notice: 'Plant was successfully created.' }
         format.json { render :show, status: :created, location: @plant }
       else
         format.html { render :new }
@@ -44,12 +42,14 @@ class PlantsController < ApplicationController
     end
   end
 
-  # PATCH/PUT /plants/1
-  # PATCH/PUT /plants/1.json
+  # PATCH/PUT companies/:company_id/plants/1
+  # PATCH/PUT companies/:company_id/plants/1.json
   def update
+
+    @plant.system_size = params[:plant][:system_size].split(' ').map(&:to_i)
     respond_to do |format|
       if @plant.update(plant_params)
-        format.html { redirect_to company_plant_path(@plant), notice: 'Plant was successfully updated.' }
+        format.html { redirect_to company_plant_path(@company, @plant), notice: 'Plant was successfully updated.' }
         format.json { render :show, status: :ok, location: @plant }
       else
         format.html { render :edit }
@@ -58,14 +58,14 @@ class PlantsController < ApplicationController
     end
   end
 
-  # DELETE /plants/1
-  # DELETE /plants/1.json
+  # DELETE companies/:company_id/plants/1
+  # DELETE companies/:company_id/plants/1.json
   def destroy
     @plant.active = false
     @plant.save
 
     respond_to do |format|
-      format.html { redirect_to company_path, notice: 'Plant was successfully destroyed.' }
+      format.html { redirect_to company_plants_path(@company), notice: 'Plant was successfully destroyed.' }
       format.json { head :no_content }
     end
   end
