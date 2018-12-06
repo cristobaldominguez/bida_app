410f75ff1a0e972f1eecdc83841ea84cf4722879
Cristobal Dominguez <cristobald@gmail.com>
2018-12-05 21:58:57 -0300

    Seed Improvements

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,35 +6,47 @@ if Rails.env == 'development'
   # Countries
   Country.destroy_all
   countries = ['Australia', 'Chile', 'United States', 'New Zealand', 'Peru']
-
   countries.sort { |a, b| a <=> b }.each do |country|
     Country.create!(name: country)
   end
 
-  industries = ['Beverage', 'Brewery', 'Dairy', 'Domestic Sewage', 'Energy', 'Food Processor', 'Goverment', 'Grower', 'Milk Processor', 'Municipality', 'Real State', 'Real State/Households/Hotels', 'Seafood Processor', 'Slaughterhouse', 'University', 'Waste Management', 'Winery', 'WWT Provider']
+  # Industries
+  Industry.destroy_all
+  industries = ['Beverage', 'Brewery', 'Dairy', 'Domestic Sewage', 'Energy', 'Food Processor',
+                'Goverment', 'Grower', 'Milk Processor', 'Municipality', 'Real State',
+                'Real State/Households/Hotels', 'Seafood Processor', 'Slaughterhouse',
+                'University', 'Waste Management', 'Winery', 'WWT Provider']
   industries.sort { |a, b| a <=> b }.each do |industry|
     Industry.create!(name: industry)
   end
 
+  # Discharge Points
+  DischargePoint.destroy_all
   discharge_point = ['Irrigation', 'Stream', 'Land Application', 'Infiltration', 'Pond', 'Sewer', 'Other']
   discharge_point.sort { |a, b| a <=> b }.each do |point|
     DischargePoint.create!(name: point)
   end
 
+  # Incident Types
+  IncidentType.destroy_all
   incidents = ['ph Imbalance', 'Worms Mortality', 'Odors', 'Run off Chemicals', 'Flooding']
   incidents.each do |incident|
     IncidentType.create!(name: incident)
   end
 
+  # Statuses
+  Status.destroy_all
   statuses = ['Open', 'Fixed', 'Being Remedied']
   statuses.each do |status|
     Status.create!(name: status)
   end
 
+  # Priorities
+  Priority.destroy_all
   priorities = ['Within 24 Hours', 'Within 3 Days', 'Within 7 Days', 'Within 30 Days']
   priorities.each do |priority|
     Priority.create!(name: priority)
   end
 
-  puts 'Added Seeds!'
+  puts 'Seeds Added!'
 end
