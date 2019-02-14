c4cf58c06e6d9e0b6b9054747e24f050a690bb38
Cristobal Dominguez <cristobald@gmail.com>
2019-02-14 20:00:42 -0300

    Add a Main Nav to Application Layout

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,17 @@
 class ApplicationController < ActionController::Base
   before_action :authenticate_user!
   before_action :configure_permitted_parameters, if: :devise_controller?
+  before_action :set_user_plants, :set_plant
 
   protected
+
+  def set_plant
+    @plant = params[:plant_id].present? ? Plant.find(params[:plant_id]) : params[:controller] == 'plants' && params[:id].present? ? Plant.find(params[:id]) : nil
+  end
+
+  def set_user_plants
+    @user_plants = current_user.plants unless current_user.nil?
+  end
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up) { |u| u.permit(:name, :lastname, :address01, :address02, :phone, :mobile, :email, :password) }
