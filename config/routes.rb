c995c392ff5bb1a3517b2929693d678f736b9a78
Cristobal Dominguez <cristobald@gmail.com>
2019-02-22 11:48:40 -0300

    Add Option for Allow User to Change Own Password

--- config/routes.rb
+++ config/routes.rb
@@ -23,7 +23,11 @@ Rails.application.routes.draw do
 
   resources :work_summaries, only: :destroy
 
-  devise_for :users
+  devise_for :users, skip: :registrations
+  as :user do
+    get 'users/edit' => 'devise/registrations#edit', :as => 'edit_user_registration'
+    put 'users' => 'devise/registrations#update', :as => 'user_registration'
+  end
   resources :users
 
   devise_scope :user do
