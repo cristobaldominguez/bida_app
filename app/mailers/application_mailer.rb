8af46f98ba334e52d431dabbeb43923ec7d509a2
Cristobal Dominguez <cristobald@gmail.com>
2019-02-25 16:39:41 -0300

    Add Devise Email Layout

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,4 +1,4 @@
 class ApplicationMailer < ActionMailer::Base
-  default from: 'from@example.com'
+  default from: 'bida@biofiltro.com'
   layout 'mailer'
 end
--- config/application.rb
+++ config/application.rb
@@ -15,5 +15,9 @@ module BIDAApp
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
+
+    config.to_prepare do
+      Devise::Mailer.layout 'mailer'
+    end
   end
 end
