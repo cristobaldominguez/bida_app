88100a844a8e7cbfdb0c489aa80f33b791ff3c5d
Cristobal Dominguez <cristobald@gmail.com>
2019-03-20 11:05:57 -0300

    Add a Datepicker to Logbook

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -18,6 +18,7 @@
 //= require popper
 //= require bootstrap-material-design
 //= require chartkick
+//= require pikaday
 //= require_tree .
 
 $(document).ready(function() { $('body').bootstrapMaterialDesign(); });
