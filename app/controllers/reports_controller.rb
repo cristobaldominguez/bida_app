4f46a334f7e4ec3c4e2f0c2772c695bc2b4ddaf3
Cristobal Dominguez <cristobald@gmail.com>
2019-04-04 12:40:53 -0300

    Add Last Tables to Report and Improvements on Page Load

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -31,7 +31,6 @@ class ReportsController < ApplicationController
 
     @graphs = @report.graphs
     set_graphs_data(@report)
-
   end
 
   # GET /reports/1/edit
@@ -101,77 +100,119 @@ class ReportsController < ApplicationController
   end
 
   def set_graphs_data(report)
-    start_date = report.date.beginning_of_month
-    end_date = report.date.end_of_month
+    # Common variables
+    report_date = report.date
+    start_date = report_date.beginning_of_month
+    end_date = report_date.end_of_month
     minimum_liquid_required = 0.05 * @plant.flow_design
-    @monthly_flow = @plant.flows.where('date BETWEEN ? AND ?', (start_date - 11.months), end_date).group_by_month(:date).sum(:value).map { |k, v| [k.strftime('%b, %Y'), v] }
-    @daily_flow = @plant.flows.where('date BETWEEN ? AND ?', start_date, end_date).order(:id).pluck(:value, :date).map { |prim, seg| [seg.strftime('%d/%m'), prim] }
-    # sampling_list = @plant.sampling_lists.includes(:access, samplings: [standard: :option]).where('created_at BETWEEN ? AND ?', start_date, end_date).select {|sl| sl.access.name == 'Internal' }.first
+    outputs = %w[In Out]
 
-    sampling_lists = @plant.sampling_lists.includes(:access, samplings: [standard: :option])
-    # sampling_lists = @plant.sampling_lists
+    # Database info
+    flows_history = @plant.flows
 
-    # @monthly_water
-    current_sampling_list = sampling_lists.select { |n| n.created_at.between?(start_date, end_date) }.select { |sl| sl.access.name == 'Internal' }.first
-    monthly_water_values = current_sampling_list.samplings.group_by { |s| s.standard.option.name }.map do |key, value|
-      value_in = value.map(&:value_in).sum
-      value_out = value.map(&:value_out).sum
-      percent = (100 - (value_out * 100) / value_in).round(1)
-      { key.to_sym => [value_in, value_out, percent.to_s + '%'] }
+    flows_year = flows_history.select { |flow| flow.date > start_date - 11.months && flow.date < end_date }
+    flows_month = flows_history.select { |flow| flow.date >= start_date && flow.date <= end_date }
+
+    treated_water_history = flows_history.reject { |flow| flow.date > end_date || flow.value < minimum_liquid_required }.group_by_month(&:date)
+    sampling_lists_history = @plant.sampling_lists.includes(samplings: [standard: :option]).created_before(end_date)
+    sampling_lists_labs = sampling_lists_history.lab
+
+    samplings_lab = sampling_lists_labs.map(&:samplings).flatten
+    samplings_grouped_by_option = samplings_lab.group_by { |sampling| sampling.standard.option.name }
+
+    lifetime_analysis_headers = samplings_grouped_by_option.map { |key, _| key }.map { |key| ["#{key} In", "#{key} Out", 'Removal'] }.flatten
+
+    year_samplings = year_samplings(sampling_lists_labs, start_date)
+    year_samplings_grouped_by_option = year_samplings.group_by { |sampling| sampling.standard.option.name }
+
+    # View Variables
+    @yearly_flow = yearly_water_flow(flows_year)
+    @monthly_flow = monthly_water_flow(flows_month)
+    @monthly_water_analysis = { "headers": ['', 'BIDA In (mg/l)', 'BIDA Out (mg/l)', 'Removal'], "data": monthly_water_analysis_data(samplings_grouped_by_option) }
+    @treated_water = { "headers": ['', 'Month', 'Year', 'Lifetime'], "data": monthly_water_flow_analysis(treated_water_history) }
+    @lifetime_analysis = { "headers": [['', 'Biological Oxygen Demand (mg/l)', 'Total Suspended Solids (mg/l)'], lifetime_analysis_headers],
+                           "average": samplings_average(samplings_grouped_by_option), "peak": samplings_peak(samplings_grouped_by_option) }
+    @year_samplings_analysis = { 'headers': [['', 'Biological Oxygen Demand (mg/l)', 'Total Suspended Solids (mg/l)'], ['Grab Date', lifetime_analysis_headers].flatten], 'data': year_samplings_tabledata(sampling_lists_labs, start_date), 'footer': [] }
+    @year_samplings_grouped_by_option = outputs.map { |output| year_samplings_grouped_by_option.map { |key, values| { name: "#{key} #{output}", data: values.map { |sampling| [sampling.date.strftime('%b, %Y'), output == 'In' ? sampling.value_in : sampling.value_out] } } } }.flatten
+  end
+
+  def percent(value_in, value_out)
+    (100 - (value_out * 100) / value_in).round(0)
+  end
+
+  def yearly_water_flow(flows_year)
+    flows_year_grouped_by_month = flows_year.group_by_month(&:date)
+    flows_year_grouped_by_month.map { |key, monthly_flows| [key.strftime('%b, %y'), monthly_flows.sum(&:value).round(0)] }
+  end
+
+  def monthly_water_flow(flows_month)
+    flows_month.map { |flow| [flow.date.strftime("%b, #{flow.date.day.ordinalize}"), flow.value] }
+  end
+
+  def treated_water(data)
+    hash = {}
+    %i[total irrigated peak].each do |key|
+      hash[key] = { last_month: data[key].last.round(0),
+                    last_year: data[key].last(12).sum.round(0),
+                    lifetime: data[key].sum.round(0) }
     end
+    hash[:average] = { last_month: (hash[:total][:last_month] / hash[:irrigated][:last_month]),
+                       last_year: (hash[:total][:last_year] / hash[:irrigated][:last_year]),
+                       lifetime: (hash[:total][:lifetime] / hash[:irrigated][:lifetime]) }
+    hash
+  end
 
-    # @treated_water
-    treated_water = @plant.flows.reject { |flow| flow.date > end_date }.reject { |flow| flow.value < minimum_liquid_required }.group_by_month(&:date)
-    treated_water_total_by_month = treated_water.map { |_, group| group.sum(&:value) }
-    treated_water_irrigated_by_month = treated_water.map { |_, group| group.size }
-    treated_water_peak_by_month = treated_water.map { |_, group| group.max_by(&:value) }.map(&:value)
-
-    treated_water = {
-      total: {
-        last_month: treated_water_total_by_month.last.round(1),
-        last_year: treated_water_total_by_month.last(12).sum.round(1),
-        lifetime: treated_water_total_by_month.sum.round(1)
-      },
-      irrigated: {
-        last_month: treated_water_irrigated_by_month.last.round(1),
-        last_year: treated_water_irrigated_by_month.last(12).sum.round(1),
-        lifetime: treated_water_irrigated_by_month.sum.round(1)
-      },
-      peak: {
-        last_month: treated_water_peak_by_month.last.round(1),
-        last_year: treated_water_peak_by_month.last(12).sum.round(1),
-        lifetime: treated_water_peak_by_month.sum.round(1)
-      }
+  def monthly_water_flow_analysis(treated_water)
+    treated_water_data = {
+      total: treated_water.map { |_, group| group.sum(&:value).round(0) },
+      irrigated: treated_water.map { |_, group| group.size },
+      peak: treated_water.map { |_, group| group.max_by(&:value) }.map(&:value)
     }
 
-    treated_water_average = {
-      last_month: (treated_water[:total][:last_month] / treated_water[:irrigated][:last_month]).round(0),
-      last_year: (treated_water[:total][:last_year] / treated_water[:irrigated][:last_year]).round(0),
-      lifetime: (treated_water[:total][:lifetime] / treated_water[:irrigated][:lifetime]).round(0)
-    }
+    treated_water = treated_water(treated_water_data)
 
-    treated_water_data = [{ "Total #{@plant.metrics.volume.pluralize}" => [treated_water[:total][:last_month], treated_water[:total][:last_year], treated_water[:total][:lifetime]] },
-                          { 'Days Irrigated' => [treated_water[:irrigated][:last_month], treated_water[:irrigated][:last_year], treated_water[:irrigated][:lifetime]] },
-                          { 'Average GPD' => [treated_water_average[:last_month], treated_water_average[:last_year], treated_water_average[:lifetime]] },
-                          { 'Peak Flow' => [treated_water[:peak][:last_month], treated_water[:peak][:last_year], treated_water[:peak][:lifetime]] }]
+    [{ "Total #{@plant.metrics.volume.pluralize}" => [treated_water[:total][:last_month], treated_water[:total][:last_year], treated_water[:total][:lifetime]] },
+     { 'Days Irrigated' => [treated_water[:irrigated][:last_month], treated_water[:irrigated][:last_year], treated_water[:irrigated][:lifetime]] },
+     { 'Average Flow (GPD)' => [treated_water[:average][:last_month], treated_water[:average][:last_year], treated_water[:average][:lifetime]] },
+     { 'Peak Flow (GPD)' => [treated_water[:peak][:last_month], treated_water[:peak][:last_year], treated_water[:peak][:lifetime]] }]
+  end
 
-    # Lifetime Water Analysis Overview
-    current_sampling_lists = sampling_lists.select { |sampling_list| sampling_list.access.name == 'Internal' }
-    grouped_samplings = current_sampling_lists.map { |sl| sl.samplings.map { |sampling| sampling } }.flatten.group_by { |sampling| sampling.standard.option.name }
-    grouped_by_month = grouped_samplings.map { |key, values| { key => values.group_by_month(&:created_at) } }
-    sampling_numbers = grouped_by_month.map { |elems| elems.map { |list, values| { list => values.map { |key, value| { key => [value.sum(&:value_in), value.sum(&:value_out)] } } } } }.flatten
-    lifetime_analysis = sampling_numbers.map { |arr| arr.map { |key, sampls| data_in = sampls.map { |samp| samp.map { |_, v| v[0] }.sum }; data_out = sampls.map { |samp| samp.map { |_, v| v[1] }.sum }; value_in = (data_in.map.inject(:+) / data_in.length).round(1); value_out = (data_out.map.inject(:+) / data_out.length).round(1); value_removal = (100 - (value_out * 100) / value_in).round(1); { key => { in: value_in, out: value_out, removal: value_removal } } } }.flatten
+  def samplings_average(data)
+    data.map do |key, value|
+      value_in = (value.sum(&:value_in) / value.size).round(0)
+      value_out = (value.sum(&:value_out) / value.size).round(0)
+      { key => { in: value_in, out: value_out, removal: percent(value_in, value_out) } }
+    end
+  end
 
-    # sampling_numbers.map { |data| data.map { |key, values| { key => [values.map { |info| info.map { |_, val| val[0] }.flatten }, values.map { |info| info.map { |_, val| val[0] }.flatten }] } } }
-    average_values = grouped_by_month.map { |elems| elems.map { |list, values| { list => values.map { |key, value| value_in = (value.sum(&:value_in) / value.size).round(1); value_out = (value.sum(&:value_out) / value.size).round(1); { key => [value_in, value_out] } } } } }.flatten
-    flatten_numbers = average_values.map { |data| data.map { |key, values| { key => { in: values.map { |dat| dat.map { |_, v| v[0] } }.flatten, out: values.map { |dat| dat.map { |_, v| v[1] } }.flatten } } } }.flatten
-    peak_values = flatten_numbers.map { |data| data.map { |k, v| value_in = v[:in].max; value_out = v[:out].max; value_removal = (100 - (value_out * 100) / value_in).round(1); { k => { in: value_in, out: value_out, removal: value_removal } } } }.flatten
+  def samplings_peak(data)
+    data.map do |key, value|
+      value_in = value.map(&:value_in).max.round(0)
+      value_out = value.map(&:value_out).max.round(0)
+      { key => { in: value_in, out: value_out, removal: percent(value_in, value_out) } }
+    end
+  end
 
-    @monthly_water = { "headers": ['', 'BIDA In', 'BIDA Out', 'Removal'], "data": monthly_water_values }
-    @treated_water = { "headers": ['', 'Month', 'Year', 'Lifetime'], "data": treated_water_data }
-    @lifetime_analysis = { "headers": [['', 'Biological Oxygen Demand', 'Total Suspended Solids'], ['BOD In', 'BOD Out', 'Removal', 'TSS In', 'TSS Out', 'Removal']], "average": lifetime_analysis, "peak": peak_values }
+  def monthly_water_analysis_data(data)
+    current_samplings = data.map { |_, samplings| samplings.last }
+    current_samplings.map do |sampling|
+      value_in = sampling.value_in.round(0)
+      value_out = sampling.value_out.round(0)
+      { sampling.standard.option.name => [value_in, value_out, percent(value_in, value_out).to_s + '%'] }
+    end
+  end
 
-    # raise
+  def year_samplings_tabledata(data, start_date)
+    year_samplings_data = year_samplings(data, start_date)
+    year_samplings_grouped_by_month = year_samplings_data.group_by_month(&:date)
+    year_samplings_grouped_by_month.map do |key, samplings|
+      { key.strftime('%B, %y') => samplings.map { |sampling| { in: sampling.value_in.round(0), out: sampling.value_out.round(0), removal: percent(sampling.value_in, sampling.value_out).to_s + '%' } } }
+    end
+  end
+
+  def year_samplings(data, start_date)
+    end_date = start_date - 11.months
+    data.select { |sampling_list| sampling_list.date > end_date }.map(&:samplings).flatten
   end
 
   def report_params
--- app/models/sampling.rb
+++ app/models/sampling.rb
@@ -1,4 +1,6 @@
 class Sampling < ApplicationRecord
   belongs_to :standard
   belongs_to :sampling_list
+
+  scope :created_before, ->(end_date) { where('sampling_lists.created_at < ?', end_date) }
 end
