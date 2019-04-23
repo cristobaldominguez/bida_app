a233641bdac392ca73f7cff960458c074ab347c8
Cristobal Dominguez <cristobald@gmail.com>
2019-04-23 17:52:17 -0400

    Update with New Menu Items Order

--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -6,6 +6,7 @@ class ApplicationRecord < ActiveRecord::Base
   scope :active, -> { where active: true }
   scope :inactive, -> { where active: false }
   scope :sort_by_id, -> { order('id ASC') }
+  scope :created_between, ->(start_date, end_date) { where('date BETWEEN ? AND ?', start_date, end_date) }
 
   def inactive!
     self.active = false
--- app/models/report.rb
+++ app/models/report.rb
@@ -5,7 +5,5 @@ class Report < ApplicationRecord
 
   enum state: %i[draft published]
 
-  scope :created_between, ->(start_date, end_date) { where('date BETWEEN ? AND ?', start_date, end_date) }
-
   accepts_nested_attributes_for :graphs, allow_destroy: true
 end
