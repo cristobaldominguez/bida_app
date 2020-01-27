bb01a7c58c2d75f6c7d8490d717745b92ee55851
Cristobal Dominguez <cristobald@gmail.com>
2020-01-27 17:40:05 -0300

    Updates Date Format

--- config/initializers/time_formats.rb
+++ config/initializers/time_formats.rb
@@ -1,3 +1,4 @@
 Time::DATE_FORMATS[:custom_datetime] = ->(time) { time.strftime("%B #{time.day.ordinalize}") }
 
-Date::DATE_FORMATS[:custom] = ->(time) { time.strftime("%B #{time.day.ordinalize}, %Y") }
+Date::DATE_FORMATS[:english] = ->(date) { date.strftime("%B #{date.day.ordinalize}, %Y") }
+# .to_s(:long_ordinal)
