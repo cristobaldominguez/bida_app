5c10fe3ca1e460c0ae842109dcb0271f8c7b7705
Cristobal Dominguez <cristobald@gmail.com>
2020-03-25 18:18:54 -0300

    Improves Task's Seeds

--- app/models/task.rb
+++ app/models/task.rb
@@ -4,5 +4,5 @@ class Task < ApplicationRecord
   enum season: %i[no show hide]
   enum frecuency: %w[daily weekly every_2_weeks monthly every_x_months]
   enum input_type: ['checkbox', 'number', 'date', 'yes/no', 'Low/Medium/High Density', 'Low/Medium/Strong Odor']
-  enum data_type: %w[other pH PSI gallons liters COD %]
+  enum data_type: %w[other pH PSI gallons liters COD % inches]
 end
