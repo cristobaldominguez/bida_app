8dc4638a43d3f942a41c25db382c7709efd74c72
Cristobal Dominguez <cristobald@gmail.com>
2020-06-24 18:00:50 -0400

    Adds some Translations Improvements

--- app/mailers/notification_mailer.rb
+++ app/mailers/notification_mailer.rb
@@ -3,29 +3,20 @@ class NotificationMailer < ApplicationMailer
     @user = user
     @alert = alert
 
-    mail(
-      to: @user.email,
-      subject: "[BIDA] › Ticket Alert #{@alert.subject}"
-    )
+    mail( to: @user.email, subject: I18n.t(:notification_subject, scope: :alert, text: @alert.subject) )
   end
 
   def support_notification(user, support)
     @user = user
     @support = support
 
-    mail(
-      to: @user.email,
-      subject: "[BIDA] › Ticket Support: #{@support.number}"
-    )
+    mail(to: @user.email, subject: I18n.t(:notification_subject, scope: :support, text: @support.number))
   end
 
   def inspection_notification(user, alert)
     @user = user
     @inspection = alert
 
-    mail(
-      to: @user.email,
-      subject: "[BIDA] › Ticket Inspection: #{@inspection.title}"
-    )
+    mail( to: @user.email, subject: I18n.t(:notification_subject, scope: :inspection, text: @inspection.title) )
   end
 end
