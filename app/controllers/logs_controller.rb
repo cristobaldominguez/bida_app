7f47ed842d735400af6137d4b052ecaab9d03c7e
Cristobal Dominguez <cristobald@gmail.com>
2020-06-25 18:29:29 -0400

    Adds an Option to Generate Logs From an Array of Tasks

--- app/controllers/logs_controller.rb
+++ app/controllers/logs_controller.rb
@@ -8,11 +8,30 @@ class LogsController < ApplicationController
 
   def self.log_creation(logbook, actual_date)
     tasks = logbook.task_list.tasks
-    new_logs = tasks.map do |task|
+
+    generate_log(tasks, logbook, actual_date).reject(&:nil?)
+  end
+
+  def self.generate_log(tasks, logbook, actual_date)
+    tasks.map do |task|
       log = Log.new(logbook: logbook, value: '', date: actual_date, task: task)
       LogCheck.new(log, actual_date).generate? ? log : nil
     end
+  end
 
-    new_logs.reject(&:nil?)
+  # Genera tasks a partir de un array de Tasks
+  def self.generate_logs(tasks, logbook, current_date)
+    @@tasks = tasks
+    @@logbook = logbook
+    days_of_month = current_date.week_split.flatten.reject(&:nil?)
+
+    logs = days_of_month.map { |day| log_creation_from_task(tasks, Date.new(current_date.year, current_date.month, day)) }
+    logs.flatten.map(&:save)
+  end
+
+  def self.log_creation_from_task(tasks, actual_date)
+    new_logs = tasks.map do |task|
+      log = generate_log(tasks, @@logbook, actual_date).reject(&:nil?)
+    end
   end
 end
