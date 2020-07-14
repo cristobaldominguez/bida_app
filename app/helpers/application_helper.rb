ace64b7ab79b2b2ac1083ae8f198d87af1449bda
Cristobal Dominguez <cristobald@gmail.com>
2020-07-14 17:12:18 -0400

    Adds Locale Helpers

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,13 @@
 module ApplicationHelper
+  def current_locale
+    (params[:locale] || I18n.default_locale).to_sym
+  end
+
+  def localize(i18n)
+    i18n[current_locale].present? ? i18n[current_locale] : i18n[I18n.default_locale]
+  end
+
+  def any_locale?(i18n)
+    I18n.available_locales.map { |locale| i18n[locale].present? }.any?
+  end
 end
