d746223245374157d0926d9db35a9cb42e2b91e6
Cristobal Dominguez <cristobald@gmail.com>
2020-07-27 12:32:08 -0400

    Improves Responsible adding name_by_id Method

--- app/models/responsible.rb
+++ app/models/responsible.rb
@@ -4,14 +4,18 @@ class Responsible
   end
 
   def self.get_list(plant)
-    [[-1, 'BioFiltro'], [0, plant.company.name]] + User.filtered_by(plant).sort.map {|u| [u.id, u.full_name] }
+    [[-1, 'BioFiltro'], [0, plant.company.name]] + User.filtered_by(plant).sort.map { |u| [u.id, u.full_name] }
   end
 
   def self.get_complete_list(plant)
-    [[-1, 'BioFiltro'], [0, plant.company.name]] + User.filtered.sort.map {|u| [u.id, u.full_name] }
+    [[-1, 'BioFiltro'], [0, plant.company.name]] + User.filtered.sort.map { |u| [u.id, u.full_name] }
   end
 
   def self.name(task)
-    get_complete_list(task.task_list.plant).select {|r| r.first == task.responsible }.last.last
+    get_complete_list(task.task_list.plant).select { |r| r.first == task.responsible }.last.last
+  end
+
+  def self.name_by_id(id)
+    User.find(id)
   end
 end
