2bd4c917af8d74f0bbb74ae83f4342a6c318cbf3
Cristobal Dominguez <cristobald@gmail.com>
2020-08-18 17:12:44 -0400

    Code Refactoring

--- app/models/processing/task_dates_creation.rb
+++ app/models/processing/task_dates_creation.rb
@@ -80,24 +80,13 @@ module Processing
     end
 
     def complete_day_name(day)
-      case day
-      when 'mon' then 'monday'
-      when 'tue' then 'tuesday'
-      when 'wed' then 'wednesday'
-      when 'thu' then 'thursday'
-      when 'fri' then 'friday'
-      when 'sat' then 'saturday'
-      when 'sun' then 'sunday'
-      end
+      days = { 'mon': 'monday', 'tue': 'tuesday', 'wed': 'wednesday', 'thu': 'thursday', 'fri': 'friday', 'sat': 'saturday', 'sun': 'sunday' }
+      days[day]
     end
 
     def num_in_words(num)
-      case num
-      when 1 then 'first'
-      when 2 then 'second'
-      when 3 then 'third'
-      when 4 then 'fourth'
-      end
+      numbers = ['', 'first', 'second', 'third', 'fourth']
+      numbers[num]
     end
   end
 end
