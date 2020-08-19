3ad35da64622e699c051f87ea6a6932b8abc4722
Cristobal Dominguez <cristobald@gmail.com>
2020-08-19 12:06:26 -0400

    Adds ToDo Daily Notifications Job

---
+++ app/jobs/todo_daily_notifications_job.rb
@@ -0,0 +1,7 @@
+class TodoDailyNotificationsJob < ApplicationJob
+  queue_as :default
+
+  def perform(*args)
+    # Do something later
+  end
+end
---
+++ test/jobs/todo_daily_notifications_job_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class TodoDailyNotificationsJobTest < ActiveJob::TestCase
+  # test "the truth" do
+  #   assert true
+  # end
+end
