f1404551a8efe7cb9224259f8cd775611ed632ad
Cristobal Dominguez <cristobald@gmail.com>
2020-11-12 11:05:33 -0300

    Adds 5 Seconds Sleep to Logbook Creation

--- app/jobs/generate_plants_logbooks_job.rb
+++ app/jobs/generate_plants_logbooks_job.rb
@@ -4,6 +4,7 @@ class GeneratePlantsLogbooksJob < ApplicationJob
   def perform
     Plant.active.sort.map do |plant|
       plant.logbooks.create(task_list: plant.task_lists.active.last)
+      sleep 5
     end
   end
 end
